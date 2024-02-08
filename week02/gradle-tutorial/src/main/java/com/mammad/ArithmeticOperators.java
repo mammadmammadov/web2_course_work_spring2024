af3e4f910d2b3bcfd4809c6ace5d47887c96f840
Mammad Mammadov <73391896+mammadmammadov@users.noreply.github.com>
2024-02-08 10:38:20 +0400

    Gradle Tutorial

---
+++ week02/gradle-tutorial/src/main/java/com/mammad/ArithmeticOperators.java
@@ -0,0 +1,14 @@
+package com.mammad;
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        //initializing variables
+        int a = 4;
+        int b = 2;
+
+        System.out.println(a + b); //addition | 6
+        System.out.println(a - b); //subtraction | 2
+        System.out.println(a * b); //multiplication | 8
+        System.out.println(a / b); //division | 2
+        System.out.println(a % b); //modulus | 0
+    }
+}
---
+++ week02/gradle-tutorial/src/test/java/com/mammad/ArithmeticOperatorsTest.java
@@ -0,0 +1,11 @@
+package com.mammad;
+
+import org.junit.Test;
+
+public class ArithmeticOperatorsTest{
+
+@Test
+public void verifyNoExceptionThrown(){
+ArithmeticOperators.main(new String[]{});
+}
+}
